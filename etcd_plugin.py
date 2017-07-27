d415e6e7f8282afa5dd36c02fe81c72b9c46a772
Change Me <akash@signalfx.com>
2017-07-27 13:02:13 -0400

    unit tests and code changes

--- etcd_plugin.py
+++ etcd_plugin.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import requests
 import collections
 import collectd
@@ -61,12 +62,10 @@ LEADER_METRICS_LATENCY = {
 }
 
 
-def read_config(conf, testing="no"):
+def read_config(conf):
     '''
     Reads the configurations provided by the user
     '''
-    collectd.info("Starting read_config")
-
     plugin_conf = {}
     interval = DEFAULT_INTERVAL
     custom_dimensions = {}
@@ -77,43 +76,43 @@ def read_config(conf, testing="no"):
 
     required_keys = {'Host', 'Port', 'Cluster'}
     optional_keys = {'Interval', 'Dimension', 'EnhancedMetrics', 'IncludeMetric', 'ExcludeMetric'}
-    ssl_keys = {'ssl_keyfile': None, 'ssl_certificate': None, 'ssl_cert_validation': None, 'ssl_ca_certs': None}
-
+    ssl_keys = {}
+    testing = False
     for val in conf.children:
         if val.key in required_keys:
             plugin_conf[val.key] = val.values[0]
         elif val.key in optional_keys and val.key == 'Interval' and val.values[0]:
             interval = val.values[0]
-        elif val.key in optional_keys and val.key == 'Dimension' and len(val.values) == 2:
-            custom_dimensions.update({val.values[0]: val.values[1]})
+        elif val.key in optional_keys and val.key == 'Dimension':
+            if len(val.values) == 2:
+                custom_dimensions.update({val.values[0]: val.values[1]})
+            else:
+                collectd.warning("WARNING: Check configuration setting for %s" % val.key)
         elif val.key in optional_keys and val.key == 'EnhancedMetrics' and val.values[0]:
             enhanced_metrics = str_to_bool(val.values[0])
         elif val.key in optional_keys and val.key == 'IncludeMetric' and val.values[0]:
             include_optional_metrics.add(val.values[0])
         elif val.key in optional_keys and val.key == 'ExcludeMetric' and val.values[0]:
             exclude_optional_metrics.add(val.values[0])
-        elif val.key in ssl_keys and val.key == 'ssl_keyfile' and val.values[0]:
+        elif val.key == 'ssl_keyfile' and val.values[0]:
             ssl_keys['ssl_keyfile'] = val.values[0]
-        elif val.key in ssl_keys and val.key == 'ssl_certificate' and val.values[0]:
+        elif val.key == 'ssl_certificate' and val.values[0]:
             ssl_keys['ssl_certificate'] = val.values[0]
-        elif val.key in ssl_keys and val.key == 'ssl_cert_validation' and val.values[0]:
+        elif val.key == 'ssl_cert_validation' and val.values[0]:
             ssl_keys['ssl_cert_validation'] = val.values[0]
-        elif val.key in ssl_keys and val.key == 'ssl_ca_certs' and val.values[0]:
+        elif val.key == 'ssl_ca_certs' and val.values[0]:
             ssl_keys['ssl_ca_certs'] = val.values[0]
-
-    for key, param in ssl_keys.items():
-        if param is None:
-            collectd.info("INFO: (%s) not provided in Configuration" % key)
-            del ssl_keys[key]
+        elif val.key == 'Testing' and str_to_bool(val.values[0]):   # if tesing is set to true
+            testing = True
 
     collectd.info("INFO: Configuration settings:")
 
     for key in required_keys:
-        val = plugin_conf[key]
-        if val is None:
-            collectd.error("ERROR: Missing required key: %s (%s)" % (key, e))
-            raise ValueError("Missing required config setting: %s" % key)
-        collectd.info("%s : %s" % (key, val))
+        try:
+            val = plugin_conf[key]
+            collectd.info("%s : %s" % (key, val))
+        except KeyError, e:
+            raise KeyError("Missing required config setting: %s" % key)
 
     base_url = ("http://%s:%s" % (plugin_conf['Host'], plugin_conf['Port']))
     module_config = {
@@ -127,10 +126,13 @@ def read_config(conf, testing="no"):
             'custom_dimensions': custom_dimensions,
             'enhanced_metrics': enhanced_metrics,
             'include_optional_metrics': include_optional_metrics,
-            'exclude_optional_metrics': exclude_optional_metrics
+            'exclude_optional_metrics': exclude_optional_metrics,
+            'Testing': testing
             }
 
-    if testing == "yes":
+    collectd.info("INFO: module_config: (%s)" % str(module_config))
+
+    if testing:
         return module_config
 
     collectd.register_read(read_metrics, interval, data=module_config, name=module_config['member_id'])
@@ -149,7 +151,6 @@ def read_metrics(data):
     '''
     Registered read call back function that collects metrics from all endpoints
     '''
-    collectd.info("Starting read_metrics")
     map_id_to_url(data, 'members')
     get_self_metrics(data, 'self')
     get_store_metrics(data, 'store')
@@ -164,6 +165,7 @@ def map_id_to_url(data, endpoint):
     etcd uses interval id for each member. This method maps the id to corresponding
     base url
     '''
+    collectd.debug("DEBUGGING: %s" % data)
     url = ("%s/v2/%s" % (data['base_url'], endpoint))
     response = get_json_helper(data, url)
 
@@ -176,7 +178,7 @@ def get_self_metrics(data, endpoint):
     '''
     Fetches metrics from the /self endpoint
     '''
-    collectd.info("Getting metrics from self")
+    collectd.info("INFO: METRICS FROM SELF ENDPOINT")
     response = get_json(data, endpoint)
 
     if response:
@@ -194,7 +196,7 @@ def get_store_metrics(data, endpoint):
     '''
     Fetches metrics from the /store endpoint
     '''
-    collectd.info("Getting metrics from store")
+    collectd.info("INFO: METRICS FROM STORE ENDPOINT")
     response = get_json(data, endpoint)
 
     if response:
@@ -218,79 +220,76 @@ def get_leader_metrics(data, endpoint):
     '''
     Fetches metrics from the /leader endpoint
     '''
-    collectd.info("Getting metrics from leader")
+    collectd.info("INFO: METRICS FROM LEADER ENDPOINT")
     response = get_json(data, endpoint)
 
     if response:
-        try:
-            if len(response['followers']) > 0:
-                followers = response['followers']
-                for follower in followers:
-                    default_dimensions = {'state': data['state'], 'follower': data[follower][7:]}
-                    plugin_instance = prepare_plugin_instance(data, default_dimensions)
+        for follower, value in response.get('followers', {}).iteritems():
+            default_dimensions = {'state': data['state'], 'follower': data[follower][7:]}
+            plugin_instance = prepare_plugin_instance(data, default_dimensions)
 
-                    for key in LEADER_METRICS_COUNTS:
-                        if key in followers[follower]['counts']:
-                            prepare_and_dispatch_metric(LEADER_METRICS_COUNTS[key].name,
-                            followers[follower]['counts'][key], LEADER_METRICS_COUNTS[key].type, plugin_instance)
+            for key in LEADER_METRICS_COUNTS:
+                if key in value['counts']:
+                    prepare_and_dispatch_metric(LEADER_METRICS_COUNTS[key].name,
+                    value['counts'][key], LEADER_METRICS_COUNTS[key].type, plugin_instance)
 
-                    for key in LEADER_METRICS_LATENCY:
-                        if key in followers[follower]['latency']:
-                            prepare_and_dispatch_metric(LEADER_METRICS_LATENCY[key].name,
-                            followers[follower]['latency'][key], LEADER_METRICS_LATENCY[key].type, plugin_instance)
-        except KeyError, e:
-            # leader may have changed between quering the /self endpoint and now
-            collectd.warning("WARNING: %s is no longer leader (%s)" % (data['base_url'], e))
-            pass
+            for key in LEADER_METRICS_LATENCY:
+                if key in value['latency']:
+                    prepare_and_dispatch_metric(LEADER_METRICS_LATENCY[key].name,
+                    value['latency'][key], LEADER_METRICS_LATENCY[key].type, plugin_instance)
 
 
 def get_optional_metrics(data, endpoint):
     '''
     Fetches optional metrics from /metrics endpoint
     '''
-    collectd.info("Getting metrics from prometheus")
+    collectd.info("INFO: METRICS FROM METRICS ENDPOINT")
     url = ("%s/%s" % (data['base_url'], endpoint))
     response = get_text(data, url)
 
     if response:
         metrics = {}
-        for line in response.splitlines():
-            formatted = line.split(' ')
+        transform_text_to_metrics(response, metrics)
 
-            if formatted[1] == 'TYPE' and formatted[3] not in ('histogram', 'summary'):
-                metric = {'name': str(formatted[2]).replace('_', '.'), 'type': str(formatted[3])}
-                metrics[formatted[2]] = metric
-                continue
-            if len(formatted) == 2:
-                name_and_dimensions = str(formatted[0]).split('{')
-                name = formatted[0]
-                dimensions = ''
-                if len(name_and_dimensions) > 1:
-                    name = name_and_dimensions[0]
-                    dimensions = name_and_dimensions[1].replace('}', '').replace('\"', '')
-                if name in metrics:
-                    metrics[name].update({'value': float(str(formatted[1])), 'dimensions': dimensions})
+        default_dimensions = {'state': data['state']}
 
         if data['enhanced_metrics']:    # if the bool is true, then exclude metrics that are not required
             for metric in metrics:
-                print metrics[metric]
                 if metric in data['exclude_optional_metrics']:
                     continue
-                default_dimensions = {'state': data['state']}
                 plugin_instance = prepare_plugin_instance(data, default_dimensions,
                                     ('%s%s' % (',', metrics[metric]['dimensions'])))
-
                 prepare_and_dispatch_metric(metrics[metric]['name'], metrics[metric]['value'],
                                                 metrics[metric]['type'], plugin_instance)
         else:
             for metric in data['include_optional_metrics']:   # include only the required metrics
                 if metric in metrics:
-                    default_dimensions = {'state': data['state']}
                     plugin_instance = prepare_plugin_instance(data, default_dimensions,
                                         ('%s%s' % (',', metrics[metric]['dimensions'])))
-
                     prepare_and_dispatch_metric(metrics[metric]['name'], metrics[metric]['value'],
                                                 metrics[metric]['type'], plugin_instance)
+
+
+def transform_text_to_metrics(response, metrics):
+    '''
+    Transforms optional text from /metrics endpoint to metrics format
+    '''
+    for line in response.splitlines():
+        formatted = line.split(' ')
+
+        if formatted[1] == 'TYPE' and formatted[3] not in ('histogram', 'summary'):
+            metric = {'name': str(formatted[2]).replace('_', '.'), 'type': str(formatted[3])}
+            metrics[formatted[2]] = metric
+            continue
+        if len(formatted) == 2:
+            name_and_dimensions = str(formatted[0]).split('{')
+            name = formatted[0]
+            dimensions = ''
+            if len(name_and_dimensions) > 1:
+                name = name_and_dimensions[0]
+                dimensions = name_and_dimensions[1].replace('}', '').replace('\"', '')
+            if name in metrics:
+                metrics[name].update({'value': float(str(formatted[1])), 'dimensions': dimensions})
 
 
 def prepare_plugin_instance(data, default_dimensions, more_dimensions=''):
@@ -306,7 +305,6 @@ def get_json(data, endpoint):
     '''
     Returns json
     '''
-    collectd.info("Fetching %s endpoint" % endpoint)
     url = ("%s/v2/stats/%s" % (data['base_url'], endpoint))
     return get_json_helper(data, url)
 
@@ -364,7 +362,6 @@ def prepare_and_dispatch_metric(name, value, type, dimensions):
     '''
     Prepares and dispatches a metric
     '''
-    collectd.info("Starting prepare_and_dispatch_metrics")
     data_point = collectd.Values(plugin="test-etcd")
     data_point.type_instance = name
     data_point.type = type
@@ -386,18 +383,16 @@ def prepare_and_print_metric(name, value, type, dimensions):
     '''
     Prints out metrics in string format
     '''
-    collectd.info("Starting prepare_and_print_metrics")
     out = ("{ name : %s, value : %s, type : %s, dimension : %s}" % (name, value, type, dimensions))
     return out
 
 
-def format_dimensions(dimension, more=''):
+def format_dimensions(dimensions, more=''):
     '''
     Formats dimensions before fed to collectd plugin instance
     '''
-    collectd.info("Starting format_dimensions")
     formatted = []
-    formatted.extend(("%s=%s" % (k, v)) for k, v in dimension.iteritems())
+    formatted.extend(("%s=%s" % (k, v)) for k, v in dimensions.iteritems())
     return ('[%s%s]' % (str(formatted).replace('\'', '').replace(' ', '').replace("\"", '').
             replace('[', '').replace(']', ''), '' if len(more) == 1 else more))
 
---
+++ test_etcd.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+import collections
+import mock
+import sys
+import pytest
+
+import sample_responses
+
+class MockCollectd(mock.MagicMock):
+    """
+    Mocks the functions and objects provided by the collectd module
+    """
+
+    @staticmethod
+    def log(log_str):
+        print log_str
+
+    debug = log
+    info = log
+    warning = log
+    error = log
+
+
+def mock_api_call(data, url):
+    parsed_url = url.split('/')
+    return getattr(sample_responses,parsed_url[-1])   # one of the endpoints that the plugin needs
+
+
+sys.modules['collectd'] = MockCollectd()
+
+import etcd_plugin
+
+ConfigOption = collections.namedtuple('ConfigOption', ('key', 'values'))
+
+fail_mock_config_required_params = mock.Mock()
+fail_mock_config_required_params.children = [
+    ConfigOption('Host', ('localhost',)),
+    ConfigOption('Port', ('32739',)),
+    ConfigOption('Testing', ('True',))
+]
+
+
+def test_config_fail():
+    with pytest.raises(KeyError):
+        etcd_plugin.read_config(fail_mock_config_required_params)
+
+
+mock_config_enhanced_metrics_on = mock.Mock()
+mock_config_enhanced_metrics_on.children = [
+    ConfigOption('Host', ('localhost',)),
+    ConfigOption('Port', ('2379',)),
+    ConfigOption('Interval', ('10',)),
+    ConfigOption('Cluster', ('MocketcdCluster',)),
+    ConfigOption('EnhancedMetrics', ('tRue',)),
+    ConfigOption('ExcludeMetric', ('etcd_debugging_mvcc_slow_watcher_total',)),
+    ConfigOption('Testing', ('True',))
+]
+
+
+mock_config_enhanced_metrics_off = mock.Mock()
+mock_config_enhanced_metrics_off.children = [
+    ConfigOption('Host', ('localhost',)),
+    ConfigOption('Port', ('22379',)),
+    ConfigOption('Interval', ('10',)),
+    ConfigOption('Cluster', ('MocketcdCluster',)),
+    ConfigOption('IncludeMetric', ('etcd_debugging_mvcc_slow_watcher_total',)),
+    ConfigOption('Testing', ('True',))
+]
+
+
+@mock.patch('etcd_plugin.get_text', mock_api_call)
+def test_optional_metrics_on():
+    etcd_plugin.read_metrics(etcd_plugin.read_config(mock_config_enhanced_metrics_off))
+
+
+@mock.patch('etcd_plugin.get_text', mock_api_call)
+def test_optional_metrics_off():
+    etcd_plugin.read_metrics(etcd_plugin.read_config(mock_config_enhanced_metrics_on))
+
+mock_config = mock.Mock()
+mock_config.children = [
+    ConfigOption('Host', ('localhost',)),
+    ConfigOption('Port', ('2379',)),
+    ConfigOption('Interval', ('10',)),
+    ConfigOption('Cluster', ('MocketcdCluster',)),
+    ConfigOption('Testing', ('True',))
+]
+
+
+def test_default_config():
+    module_config = etcd_plugin.read_config(mock_config)
+    assert module_config['plugin_conf']['Host'] == 'localhost'
+    assert module_config['plugin_conf']['Port'] == '2379'
+    assert module_config['interval'] == '10'
+    assert module_config['base_url'] == 'http://localhost:2379'
+    assert module_config['plugin_conf']['Cluster'] == 'MocketcdCluster'
+
+
+@mock.patch('etcd_plugin.get_json_helper', mock_api_call)
+def test_read():
+    etcd_plugin.read_metrics(etcd_plugin.read_config(mock_config))
